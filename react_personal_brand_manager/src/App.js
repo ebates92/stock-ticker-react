8ac2ed3951a2a238c0a31f0f6157f9e37d537579
Evan Bates <ebates92@gmail.com>
2018-02-09 14:34:09 -0500

    added react routing

--- react_personal_brand_manager/src/App.js
+++ react_personal_brand_manager/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import StockSection from './stocksections';
-import StockDropdown from './stock-dropdown'
+import StockSection from './stock_performance/stocksections';
+import StockDropdown from './stock_performance/stock-dropdown';
 import axios from 'axios'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 
 
@@ -15,34 +16,47 @@ class App extends Component {
     this.state = {
       wantNews: false,
       data: {},
-      URL_iexTrading: 'https://api.iextrading.com/1.0/stock/market/batch?symbols=vmw&types=quote,news,chart&range=1m&last=5',
-      symbols: ['vmw']
+      URL_iexTrading: 'https://api.iextrading.com/1.0/stock/market/batch?symbols=vmw,adbe,amzn,fb,nvda,ntdoy,nflx,pstg,wmt,robo,botz,vti,vgt&types=quote,news,chart&range=1m&last=5',
+      symbols: ['VMW','ADBE','FB','NVDA','NFLX','PSTG','WMT','ROBO','BOTZ','VIT','VGT']
     }
   }
 
   render(props) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to your Stock Tracker</h1>
-        </header>
-        <div className='nav'>
-          <div className='stock performance'>Stock Performance</div>
-          <div className='Investments'>Investments</div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Your Personal Stock App</h1>
+          </header>
+          <div className='nav'>
+            <div className='stock performance'><Link to='/'>Stock Performance</Link></div>
+            <div className='Investments'><Link to='/investments'>Investments</Link></div>
+          </div>
+
+          <Route exact path='/' component = {this.StockPerformance}/>
         </div>
-        <div className='options'>
-          <StockDropdown symbols={this.state.symbols} symbolHandler={this.symbolHandler}/>
-          <form>
-            <label className="news-toggle">Want news? <input name='want-news' type='checkbox' onChange = {this.newsHandler}/></label>
-          </form>
-        </div>
-        <div className="App-intro">
-          <StockSection data={this.state.data} wantNews = {this.state.wantNews}/>
-        </div>
-      </div>
+      </Router>
     );
   }
+
+  // MAIN COMPONENTS
+
+  StockPerformance = () => (
+    <React.Fragment>
+      <div className='options'>
+        <StockDropdown symbols={this.state.symbols} symbolHandler={this.symbolHandler}/>
+        <form>
+          <label className="news-toggle">Want news? <input name='want-news' type='checkbox' onChange = {this.newsHandler}/></label>
+        </form>
+      </div>
+      <div className="App-intro">
+        <StockSection data={this.state.data} wantNews = {this.state.wantNews}/>
+      </div>
+    </React.Fragment>
+  )
+
+  // FUNCTIONS
 
   newsHandler = (event) => {
     this.setState({
